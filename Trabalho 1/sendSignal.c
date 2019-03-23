dee4b73d5aa77b95a08533e481a4212ca158d983
João André Pestre <joaoandre.pestre@poli.ufrj.br>
2019-03-23 13:23:22 -0300

    Cria verificação de processo e envio de sinal

--- Trabalho 1/sendSignal.c
+++ Trabalho 1/sendSignal.c
@@ -1,15 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char **argv){
 
+    int pid, sig, err;
+
+    // Checa se os parâmetros esperados foram passados
     if(argc!=3){
         printf("Número de parâmetros incorreto. Forneça o PID e o Sinal desejado.\n");
         return -1;
     }
 
-    int pid = atoi(argv[1]);
-    int sig = atoi(argv[2]);
-    printf("PID: %d\nSignal: %d\n",pid, sig);
+    // Recupera os parâmetros passados
+    pid = atoi(argv[1]);
+    sig = atoi(argv[2]);
+
+    // Checa se o processo escolhido existe
+    // Envio de sinal NULL ao processo falha se o processo não existe
+    if(kill(pid, 0) == -1){
+        printf("Processo %d não existe. Forneça o PID de um processo existente.\n", pid);
+        return -1;
+    }
+
+    // Envia o sinal ao processo escolhido
+    err = kill(pid, sig);
+
+    if(err == -1){
+        printf("Ocorreu um erro no envio do sinal.\n");
+        return -1;
+    }
+
+    printf("Sinal %d enviado ao processo %d.\n", sig, pid);
     return 0;
 }
