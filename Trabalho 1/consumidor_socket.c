9e77233b215bdd8860c2c2515b2dc7c15a2893e5
João André Pestre <joaoandre.pestre@poli.ufrj.br>
2019-04-02 15:29:31 -0300

    Transforma produtor em cliente e consumidor em servidor

--- Trabalho 1/consumidor_socket.c
+++ Trabalho 1/consumidor_socket.c
@@ -3,7 +3,6 @@
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <string.h> 
-#include<arpa/inet.h>
 
 #define PORT 8080 
    
@@ -19,48 +18,65 @@ int numeroPrimo(int num){
 
 int main(int argc, char const *argv[]) 
 { 
-    struct sockaddr_in address; 
-    int sock = 0, valread; 
-    struct sockaddr_in serv_addr; 
-    char *hello = "Conectado ao consumidor"; 
-    char buffer[1024] = {0};
 
     char resposta[1];
     int num_analisado;
-    
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
-        printf("\n Socket creation error \n"); 
-        return -1; 
+
+    /* Início do código de conexão com sockets
+    retirado de: https://www.geeksforgeeks.org/socket-programming-cc/ */
+
+    int server_fd, new_socket, valread; 
+    struct sockaddr_in address; 
+    int opt = 1; 
+    int addrlen = sizeof(address); 
+    char buffer[1024] = {0};  
+
+	// Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)  { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
     } 
-   
-    memset(&serv_addr, '0', sizeof(serv_addr)); 
-   
-    serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(PORT); 
        
-    // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
-    { 
-        printf("\nInvalid address/ Address not supported \n"); 
-        return -1; 
-    } 
-   
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        printf("\nConnection Failed \n"); 
-        return -1; 
+    // Forcefully attaching socket to the port 8080 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    }
+
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons( PORT ); 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
     } 
 
-    send(sock , hello , strlen(hello) , 0 ); 
+    if (listen(server_fd, 3) < 0) { 
+        perror("listen"); 
+        exit(EXIT_FAILURE); 
+    }
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,(socklen_t*)&addrlen))<0) { 
+        perror("accept"); 
+        exit(EXIT_FAILURE); 
+    }
+
+   	valread = read(new_socket, buffer, 1024);
+	printf("%s\n", buffer);
+
+    /* Fim do código de conexão com sockets
+    retirado de: https://www.geeksforgeeks.org/socket-programming-cc/ */ 
     
-    while(/* strcmp(buffer, "0\0") &&  */read(sock , buffer, 1024) != -1) {
+    while(strcmp(buffer, "0") && read(new_socket , buffer, 1024) != -1) {
         printf("Número recebido %s\n", buffer);
         
         num_analisado = strtol(buffer, NULL, 10);
 
         sprintf(resposta, "%d", numeroPrimo(num_analisado));
 
-        send(sock, resposta, 2, 0);
+        send(new_socket, resposta, 2, 0);
     }
 
     printf("Fechando conexão com o produtor\n");
--- Trabalho 1/produtor_socket.c
+++ Trabalho 1/produtor_socket.c
@@ -5,15 +5,12 @@
 #include <netinet/in.h> 
 #include <string.h>
 #include <time.h>
+#include<arpa/inet.h>
+
 
 #define PORT 8080
 
 int main(int argc, char const *argv[]) { 
-    int server_fd, new_socket, valread; 
-    struct sockaddr_in address; 
-    int opt = 1; 
-    int addrlen = sizeof(address); 
-    char buffer[1024] = {0};  
 
     int quantidade_num_gerados;
     char str_num_gerado[20];
@@ -30,41 +27,42 @@ int main(int argc, char const *argv[]) {
 
 	quantidade_num_gerados = atoi(argv[1]);
 
-	// Creating socket file descriptor 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)  { 
-        perror("socket failed"); 
-        exit(EXIT_FAILURE); 
+    /* Início do código de conexão com sockets
+    retirado de: https://www.geeksforgeeks.org/socket-programming-cc/ */
+
+    struct sockaddr_in address; 
+    int sock = 0, valread; 
+    struct sockaddr_in serv_addr; 
+    char *hello = "Conectado ao produtor"; 
+    char buffer[1024] = {0};
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
+        printf("\n Socket creation error \n"); 
+        return -1; 
     } 
+   
+    memset(&serv_addr, '0', sizeof(serv_addr)); 
+   
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT); 
        
-    // Forcefully attaching socket to the port 8080 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
-        perror("setsockopt"); 
-        exit(EXIT_FAILURE); 
-    }
-
-    address.sin_family = AF_INET; 
-    address.sin_addr.s_addr = INADDR_ANY; 
-    address.sin_port = htons( PORT ); 
-       
-    // Forcefully attaching socket to the port 8080 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    } 
+   
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnection Failed \n"); 
+        return -1; 
     } 
 
-    if (listen(server_fd, 3) < 0) { 
-        perror("listen"); 
-        exit(EXIT_FAILURE); 
-    }
+    send(sock , hello , strlen(hello) , 0 );
 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,(socklen_t*)&addrlen))<0) { 
-        perror("accept"); 
-        exit(EXIT_FAILURE); 
-    }
-
-	esperar_conexao: 
-    	valread = read(new_socket, buffer, 1024);
-		printf("%s\n", buffer);
+    /* Fim do código de conexão com sockets
+    retirado de: https://www.geeksforgeeks.org/socket-programming-cc/ */
 
 	int i;
     for(i = 0; i < quantidade_num_gerados; i++){
@@ -76,9 +74,9 @@ int main(int argc, char const *argv[]) {
         // Define mensagem a ser enviada
         sprintf(str_num_gerado, "%d", numero_aleatorio);
 
-	    send(new_socket, str_num_gerado , strlen(str_num_gerado) , 0);
+	    send(sock, str_num_gerado , strlen(str_num_gerado)+1 , 0);
 
-		valread = read(new_socket, buffer, 2);
+		valread = read(sock, buffer, 2);
 
 		if(strcmp(buffer, "1") == 0) {
 			printf("Número %d é primo\n", numero_aleatorio);
@@ -87,11 +85,9 @@ int main(int argc, char const *argv[]) {
 		}
     }    
 
-	send(new_socket , "0", 1 , 0);
+	send(sock , "0", 2 , 0);
 
     printf("Quantidade máxima de números atingida. Terminando conexão com consumidor.\n");
-
-	//goto esperar_conexao;
     
 	return 0; 
 } 
