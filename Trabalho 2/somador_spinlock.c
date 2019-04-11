6b63bd6d213e2cef5eda20815b285da45fa62c17
João André Pestre <joaoandre.pestre@poli.ufrj.br>
2019-04-11 16:33:08 -0300

    Implementa threads simultâneas

--- Trabalho 2/somador_spinlock.c
+++ Trabalho 2/somador_spinlock.c
@@ -4,15 +4,14 @@
 #include <pthread.h>
 #include "./lock/lock.h"
 
-typedef struct{
-    int *numeros;
-    int tam;
-} thread_args;
-
-void *somador_thread(thread_args* args);
+void *somador_thread(void* threadNum);
 
 Lock *lock;
 int somatorio;
+
+int* numeros;
+int inicio;
+int tamanho;
 
 int main(int argc, char **argv){
 
@@ -25,38 +24,66 @@ int main(int argc, char **argv){
     // Recupera parâmetros
     int K = atoi(argv[1]);
 
+    // Inicia variáveis globais
+
     // Inicia lock
     lock = criaLock();
-
+    
+    // Inicia somatório em 0
     somatorio = 0;
-    int numeros[5] = {1,1,1,1,1};
-    thread_args args;
-    args.numeros = numeros;
-    args.tam = 5;
 
-    printf("Somatório antes: %d\n", somatorio);
+    // Inicia array de números a serem somados
+    numeros = (int*) malloc(4*sizeof(int));
+    long i;
+    for(i=0;i<4;i++){
+        numeros[i] = i+1;
+    }
+    // Inicia tamanho do array que cada thread será responsável
+    tamanho = 4/K;
+    // Inicia inidice de partida da soma de cada thread como 0
+    inicio = 0;
+    
+
+    printf("Somatório antes: %d\n\n", somatorio);
+    
     // Cria threads
-    pthread_t thread;
-    pthread_create(&thread, NULL, somador_thread, &args);
-    pthread_join(thread, NULL);
+    pthread_t threads[K];
+    for(i=0;i<K;i++){
+        pthread_create(&threads[i], NULL, somador_thread, (void*) i);
+    }
+    
+    // Espera pelo fim das threads
+    for(i=0;i<K;i++){
+        pthread_join(threads[i], NULL);
+    }
+    
 
     printf("Somatório: %d\n", somatorio);
 
     return 0;
 }
 
-void *somador_thread(thread_args* args){
+void *somador_thread(void* threadNum){
 
-    int *numeros = args->numeros;
-    int tam = args->tam;
+    // Define o indice de início da thread a partir do seu threadNum
+    int inic = inicio + (long)threadNum*tamanho;
 
+    printf("Args:\n\tinicio: %d\n\ttam:%d\n", inic, tamanho);
+
+    // Realiza a soma parcial
     int soma = 0;
     int i;
-    for(i=0;i<tam;i++){
-        soma += numeros[i];
+    printf("Numeros: ");
+    for(i=0;i<tamanho;i++){
+        printf("%d; ", numeros[inic+i]);
+        soma += numeros[inic+i];
     }
+    printf("\n");
 
+    printf("Soma parcial: %d\n\n", soma);
+
+    // Adiciona a soma parcial ao somatório total
     acquire(lock);
-    somatorio += soma;
+    somatorio += soma; // Protegido pelo lock
     release(lock);
 }
