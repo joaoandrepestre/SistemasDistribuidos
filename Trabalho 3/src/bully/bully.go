62472bece4835eae09fbd97e9f744bdda400eaf0
joaoandrepestre <joaoandre.pestre@poli.ufrj.br>
2019-06-08 19:29:24 -0300

    Cria multiplexação de sockets, começa implementação de algoritmo
    Co-authored-by: Gabriel Mendes gabrielm.moraes@hotmail.com

--- Trabalho 3/src/bully/bully.go
+++ Trabalho 3/src/bully/bully.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
 	"strconv"
 	"strings"
 	"sync"
+	"bufio"
+	"time"
 )
 
 var pid int
@@ -16,12 +17,14 @@ var eleicao_id int
 var lider_id int
 var numero_processos int
 var listener *net.TCPListener
+var servers []*net.TCPConn
 var clients []*net.TCPConn
+var lider_vivo int
+var recebeu_ok int
 
 func main() {
 
 	var wg sync.WaitGroup
-	wg.Add(1)
 	var port int
 
 	// Recupera os argumentos da linha de comando
@@ -48,8 +51,16 @@ func main() {
 	if err != nil {
 		log.Fatal("listen error:", err)
 	}
-	fmt.Println("Socket aberta em ", port)
-	go http.Serve(listener, nil)
+	fmt.Println("Servindo em ", port)
+	// Aceita conexões e armazena no array servers
+	wg.Add(1)
+	go func(){
+		for i:=1;i<numero_processos;i++{
+			conn, _ := listener.AcceptTCP()
+			servers = append(servers, conn)
+			fmt.Println("Aceitou conexão")
+		}
+	}()
 
 	// Conecta aos sockets TCP dos outros processos
 	for i := 0; i < numero_processos; i++ {
@@ -61,34 +72,107 @@ func main() {
 			for err != nil {
 				client, err = net.DialTCP("tcp", nil, addr)
 			}
+			fmt.Println("Solicitou conexão de ", port)
 			clients = append(clients, client)
-			fmt.Println("Conectado a socket em ", port)
 		}
 	}
 
+	wg.Add(2)
+	go ReceiveMsg()
+	go CheckLider()
 	// Espera até a thread do servidor morrer
 	wg.Wait()
 }
 
+func CheckLider(){
+	for{
+		if lider_id != eleicao_id{
+			lider_vivo = 0
+
+			lider_index := lider_id
+			if lider_id > eleicao_id{
+				lider_index--
+			}
+			// Manda mensagem para lider para checar se está vivo
+			fmt.Fprintf(clients[lider_index], "4|%d|%d\n", pid, eleicao_id)
+			fmt.Println("Checou lider")
+
+			time.AfterFunc(1*time.Second, func(){
+				if lider_vivo != 1{
+					fmt.Println("Lider não respondeu")
+					// Se lider está morto, manda eleição para todos os processos
+					recebeu_ok = 0
+					for _, client := range clients{
+						fmt.Println("Enviou eleição")
+						fmt.Fprintf(client, "1|%d|%d\n", pid, eleicao_id)
+					}
+				}
+			})
+		}		
+		time.Sleep(2*time.Second)
+	}
+}
+
 func ReceiveMsg() {
-	var msg string
-	var splitMsg []string
+	var mensagem string
+	var err error
 
-	for {
-		splitMsg = strings.Split(msg, "|")
-		msgType := splitMsg[0]
+	if eleicao_id == numero_processos - 1{
+		for {
+			for _, server := range servers{
 
-		switch msgType {
-		case "1":
-			fmt.Println("Eleição")
-		case "2":
-			fmt.Println("OK")
-		case "3":
-			fmt.Println("Líder")
-		case "4":
-			fmt.Println("Vivo")
-		case "5":
-			fmt.Println("Vivo_OK")
+				mensagem, err = bufio.NewReader(server).ReadString('\n')
+				fmt.Println("Recebeu mensagem ", mensagem)
+				if err != nil {
+					log.Fatal("Read error: ", err)
+				}
+				splitMsg := strings.Split(mensagem, "|")
+				tipo, _ := strconv.Atoi(splitMsg[0])
+				//pid_mensagem, _ := strconv.Atoi(splitMsg[1])
+				eleicao_id_mensagem, _ := strconv.Atoi(splitMsg[2])
+
+				switch tipo {
+				case 1:
+					fmt.Println("Eleição")
+					eleicao(eleicao_id_mensagem)
+				case 2:
+					fmt.Println("OK")
+					recebeu_ok = 1
+				case 3:
+					fmt.Println("Líder")
+					lider_id = eleicao_id_mensagem
+				case 4:
+					fmt.Println("Vivo")
+					index := eleicao_id_mensagem
+					if eleicao_id_mensagem > eleicao_id{
+						index--
+					}
+					fmt.Fprintf(clients[index], "5|%d|%d\n", pid, eleicao_id)
+				case 5:
+					fmt.Println("Vivo_OK")
+					lider_vivo = 1
+				}
+			}
 		}
+	}
+}
+
+func eleicao(eleicao_id_mensagem int){
+	if eleicao_id > eleicao_id_mensagem {
+		fmt.Fprintf(clients[eleicao_id_mensagem], "2|%d|%d\n", pid, eleicao_id)
+		recebeu_ok = 0
+		for _, client := range clients{
+			fmt.Fprintf(client, "1|%d|%d\n", pid, eleicao_id)
+		}
+
+		go func(){
+			time.AfterFunc(1*time.Second, func(){
+			 	if recebeu_ok != 1{
+					for _, client := range clients{
+						fmt.Fprintf(client, "3|%d|%d\n", pid, eleicao_id)
+					}
+				}
+			})
+		}()
 	}
 }
