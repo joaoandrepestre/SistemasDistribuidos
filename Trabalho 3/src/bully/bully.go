4da85e711f70c7c19848425e0c0d9c1e404b4f4f
João André Pestre <joaoandre.pestre@poli.ufrj.br>
2019-06-12 14:21:08 -0300

    Começa utilização de mecanismos para sincronização de threads
    Co-authored-by: Gabriel Mendes gabrielm.moraes@hotmail.com

--- Trabalho 3/src/bully/bully.go
+++ Trabalho 3/src/bully/bully.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
@@ -8,19 +9,33 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-	"bufio"
+	"sync/atomic"
 	"time"
 )
 
-var pid int
-var eleicao_id int
-var lider_id int
-var numero_processos int
-var listener *net.TCPListener
+// ThreadSafeInt - interface para impedir condição de corrida em variáveis globais
+type ThreadSafeInt struct {
+	value int32
+}
+
+// Get - utiliza biblioteca atomic para serializar a leitura ao valor armazenado
+func (t *ThreadSafeInt) Get() int {
+	return int(atomic.LoadInt32(&t.value))
+}
+
+// Set - utiliza biblioteca atomic para serializar a escrita ao valor armazenado
+func (t *ThreadSafeInt) Set(v int) {
+	atomic.StoreInt32(&t.value, int32(v))
+}
+
+var pid ThreadSafeInt
+var eleicaoID ThreadSafeInt
+var liderID ThreadSafeInt
+var numeroProcessos ThreadSafeInt
 var servers []*net.TCPConn
 var clients []*net.TCPConn
-var lider_vivo int
-var recebeu_ok int
+var okTimer *time.Timer
+var liderTimer *time.Timer
 
 func main() {
 
@@ -29,33 +44,37 @@ func main() {
 
 	// Recupera os argumentos da linha de comando
 	args := os.Args[1:]
+	// Checa o número de parâmetros
 	if len(args) < 2 {
 		log.Fatal("Número de argumentos inválido. Forneça o número de processos e o ID do processo atual.")
-		os.Exit(1)
 	}
-	numero_processos, err := strconv.Atoi(args[0])
-	eleicao_id, err = strconv.Atoi(args[1])
-	if eleicao_id < 0 || eleicao_id >= numero_processos {
-		log.Fatal("Id para eleição inválido. Forneça um ID entre 0 e ", numero_processos-1)
-		os.Exit(1)
+	tmp, _ := strconv.Atoi(args[0])
+	numeroProcessos.Set(tmp)
+	tmp, _ = strconv.Atoi(args[1])
+	eleicaoID.Set(tmp)
+	// Checa se ID passado está dentro dos limites possíveis
+	if eleicaoID.Get() < 0 || eleicaoID.Get() >= numeroProcessos.Get() {
+		log.Fatal("Id para eleição inválido. Forneça um ID entre 0 e ", numeroProcessos.Get()-1)
 	}
 
 	// Inicia outras variáveis globais
-	pid = os.Getpid()
-	lider_id = numero_processos - 1 // Líder inicialmente é o processo de maior ID
+	pid.Set(os.Getpid())
+	liderID.Set(numeroProcessos.Get() - 1) // Líder inicialmente é o processo de maior ID
 
 	// Cria um socket TCP para o processo ouvir os outros
-	port = 4000 + eleicao_id
-	addr, err := net.ResolveTCPAddr("tcp4", ":"+strconv.Itoa(port))
-	listener, err = net.ListenTCP("tcp", addr)
+	port = 4000 + eleicaoID.Get()
+	addr, _ := net.ResolveTCPAddr("tcp4", ":"+strconv.Itoa(port))
+	listener, err := net.ListenTCP("tcp", addr)
 	if err != nil {
 		log.Fatal("listen error:", err)
 	}
 	fmt.Println("Servindo em ", port)
-	// Aceita conexões e armazena no array servers
+
 	wg.Add(1)
-	go func(){
-		for i:=1;i<numero_processos;i++{
+	// Aceita conexões e armazena no array servers
+	go func() {
+		defer wg.Done()
+		for i := 1; i < numeroProcessos.Get(); i++ {
 			conn, _ := listener.AcceptTCP()
 			servers = append(servers, conn)
 			fmt.Println("Aceitou conexão")
@@ -63,10 +82,10 @@ func main() {
 	}()
 
 	// Conecta aos sockets TCP dos outros processos
-	for i := 0; i < numero_processos; i++ {
-		if i != eleicao_id {
+	for i := 0; i < numeroProcessos.Get(); i++ {
+		if i != eleicaoID.Get() {
 			port = 4000 + i
-			addr, err = net.ResolveTCPAddr("tcp4", ":"+strconv.Itoa(port))
+			addr, _ = net.ResolveTCPAddr("tcp4", ":"+strconv.Itoa(port))
 			client, err := net.DialTCP("tcp", nil, addr)
 			// Espera até a socket estar aberta para se conectar
 			for err != nil {
@@ -76,51 +95,44 @@ func main() {
 			clients = append(clients, client)
 		}
 	}
+	// Espera todas as conexões serem aceitas
+	wg.Wait()
 
 	wg.Add(2)
-	go ReceiveMsg()
 	go CheckLider()
-	// Espera até a thread do servidor morrer
+	go ReceiveMsg()
 	wg.Wait()
 }
 
-func CheckLider(){
-	for{
-		if lider_id != eleicao_id{
-			lider_vivo = 0
+// CheckLider - função que periodicamente checa se líder está ativo
+func CheckLider() {
+	for {
+		if liderID.Get() != eleicaoID.Get() {
+			liderIndex := clientIndex(liderID.Get())
 
-			lider_index := lider_id
-			if lider_id > eleicao_id{
-				lider_index--
-			}
 			// Manda mensagem para lider para checar se está vivo
-			fmt.Fprintf(clients[lider_index], "4|%d|%d\n", pid, eleicao_id)
+			fmt.Fprintf(clients[liderIndex], "4|%d|%d\n", pid.Get(), eleicaoID.Get())
 			fmt.Println("Checou lider")
 
-			time.AfterFunc(1*time.Second, func(){
-				if lider_vivo != 1{
-					fmt.Println("Lider não respondeu")
-					// Se lider está morto, manda eleição para todos os processos
-					recebeu_ok = 0
-					for _, client := range clients{
-						fmt.Println("Enviou eleição")
-						fmt.Fprintf(client, "1|%d|%d\n", pid, eleicao_id)
-					}
-				}
+			liderTimer = time.AfterFunc(1*time.Second, func() {
+				fmt.Println("Lider não respondeu")
+				// Se lider está morto, manda eleição para todos os processos
+				enviarEleicao()
 			})
-		}		
-		time.Sleep(2*time.Second)
+		}
+		time.Sleep(5 * time.Second)
 	}
 }
 
+// ReceiveMsg - função que recebe e trata as mensagens recebidas
 func ReceiveMsg() {
 	var mensagem string
 	var err error
 
-	if eleicao_id == numero_processos - 1{
+	if eleicaoID.Get() != numeroProcessos.Get()-1 {
 		for {
-			for _, server := range servers{
-
+			for i, server := range servers {
+				fmt.Println("Lendo mensagem de: ", i)
 				mensagem, err = bufio.NewReader(server).ReadString('\n')
 				fmt.Println("Recebeu mensagem ", mensagem)
 				if err != nil {
@@ -129,50 +141,63 @@ func ReceiveMsg() {
 				splitMsg := strings.Split(mensagem, "|")
 				tipo, _ := strconv.Atoi(splitMsg[0])
 				//pid_mensagem, _ := strconv.Atoi(splitMsg[1])
-				eleicao_id_mensagem, _ := strconv.Atoi(splitMsg[2])
+				eleicaoIDMensagem, _ := strconv.Atoi(splitMsg[2])
+				fmt.Println("Recebeu mensagem de: ", eleicaoIDMensagem)
 
 				switch tipo {
 				case 1:
 					fmt.Println("Eleição")
-					eleicao(eleicao_id_mensagem)
+					go tratarEleicao(eleicaoIDMensagem)
 				case 2:
 					fmt.Println("OK")
-					recebeu_ok = 1
+					okTimer.Stop()
 				case 3:
 					fmt.Println("Líder")
-					lider_id = eleicao_id_mensagem
+					liderID.Set(eleicaoIDMensagem)
 				case 4:
 					fmt.Println("Vivo")
-					index := eleicao_id_mensagem
-					if eleicao_id_mensagem > eleicao_id{
-						index--
-					}
-					fmt.Fprintf(clients[index], "5|%d|%d\n", pid, eleicao_id)
+					index := clientIndex(eleicaoIDMensagem)
+					fmt.Fprintf(clients[index], "5|%d|%d\n", pid.Get(), eleicaoID.Get())
 				case 5:
 					fmt.Println("Vivo_OK")
-					lider_vivo = 1
+					liderTimer.Stop()
 				}
 			}
 		}
 	}
 }
 
-func eleicao(eleicao_id_mensagem int){
-	if eleicao_id > eleicao_id_mensagem {
-		fmt.Fprintf(clients[eleicao_id_mensagem], "2|%d|%d\n", pid, eleicao_id)
-		recebeu_ok = 0
-		for _, client := range clients{
-			fmt.Fprintf(client, "1|%d|%d\n", pid, eleicao_id)
-		}
-
-		go func(){
-			time.AfterFunc(1*time.Second, func(){
-			 	if recebeu_ok != 1{
-					for _, client := range clients{
-						fmt.Fprintf(client, "3|%d|%d\n", pid, eleicao_id)
-					}
-				}
-			})
-		}()
+func tratarEleicao(eleicaoIDMensagem int) {
+	if eleicaoID.Get() > eleicaoIDMensagem {
+		fmt.Println("Meu ID é maior: ", eleicaoID.Get(), " > ", eleicaoIDMensagem)
+		fmt.Fprintf(clients[eleicaoIDMensagem], "2|%d|%d\n", pid.Get(), eleicaoID.Get())
+		fmt.Println("Enviei OK para: ", eleicaoIDMensagem)
+		enviarEleicao()
+	} else {
+		fmt.Println("Meu ID é menor: ", eleicaoID.Get(), " < ", eleicaoIDMensagem)
 	}
+}
+
+func enviarEleicao() {
+	for _, client := range clients {
+		fmt.Fprintf(client, "1|%d|%d\n", pid.Get(), eleicaoID.Get())
+	}
+	fmt.Println("Enviei ELEIÇÃO para todos")
+
+	fmt.Println("Aguardando OK...")
+	okTimer = time.AfterFunc(1*time.Second, func() {
+		fmt.Println("Não recebi OK.")
+		for _, client := range clients {
+			fmt.Fprintf(client, "3|%d|%d\n", pid.Get(), eleicaoID.Get())
+		}
+		fmt.Println("Enviei LIDER para todos")
+	})
+}
+
+func clientIndex(id int) int {
+	index := id
+	if id > eleicaoID.Get() {
+		index--
+	}
+	return index
 }
